eda25e80aefaf72f4f06602963c8900017aab4ef
Wei-Hang Wang <wf6212@gmail.com>
2020-02-29 23:03:21 +0800

    add validation and modify style

--- script.js
+++ script.js
@@ -3,6 +3,11 @@
   let prevBtns = Array.from(document.querySelectorAll('.prev'));
   let finishBtn = document.querySelector('.finish');
   let stepperForm = document.getElementById('stepper-form');
+  let username = document.getElementById('username');
+  let password = document.getElementById('password');
+  let email = document.getElementById('email');
+  let phone = document.getElementById('phone');
+  let address = document.getElementById('address');
   let current_move = 0;
   const step_size = 333;
 
@@ -16,10 +21,40 @@
 
   finishBtn.addEventListener('click', handleFinishBtn);
 
+  function validate(items) {
+    let validated = true;
+    items.forEach(el => {
+      if(!el.checkValidity()) {
+        el.classList.add('is-invalid');
+        validated = false;
+      }
+      else {
+        el.classList.remove('is-invalid');
+      }
+    });
+    
+    return validated;
+  }
+
   function handleNextBtn(e) {
     e.preventDefault();
     const current_step = e.target.parentElement.parentElement.parentElement;
     const next_step = current_step.nextElementSibling;
+
+    let isOK = false;
+    
+    switch(current_step.id) {
+      case 'fieldset1':
+        isOK = validate([username, password]);
+        break;
+      case 'fieldset2':
+        isOK = validate([email]);
+        break;
+      default :
+    }
+    
+    if(!isOK) return false;
+    
     next_step.classList.add('active');
     current_step.classList.remove('active');
     current_move += -step_size;
@@ -40,8 +75,14 @@
 
   function handleFinishBtn(e) {
     let current_step = this.parentElement.parentElement.parentElement;
-    
+    const isOK = validate([phone, address]);
+    if(!isOK) {
+      e.preventDefault();
+      return false;
+    }
     if(confirm('Are you sure to submit?')) {
+      location.reload();
+      setTimeout(alert('Submit Successfully!'), 1000);
       return true;
     } else {
       e.preventDefault();
